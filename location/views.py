1dee04bc1200cff324315727af17ac6fa4802993
alexeyvershinin <og2001@yandex.ru>
2023-02-04 14:59:49 +0300

    В функции show_map проверил работу формы, добавил карту и наследование шаблонов

--- location/views.py
+++ location/views.py
@@ -1,5 +1,22 @@
 from django.shortcuts import render
+from location.forms import CadastralNumberForm
+import folium
+
 
 # Create your views here.
 def show_map(request):
-    return render(request, 'location/index.html')
+    if request.method == 'POST':
+        form = CadastralNumberForm(request.POST)
+        if form.is_valid():
+            print(form.cleaned_data['number'])
+    else:
+        form = CadastralNumberForm()
+    # карта
+    m = folium.Map(location=[64.6863136, 97.7453061], zoom_start=3)
+    m = m._repr_html_()
+
+    context = {
+        'form': form,
+        'm': m
+    }
+    return render(request, 'location/index.html', context)
