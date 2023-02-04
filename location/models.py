227e2b3f565e3a7de3dc3a0051f8f932445d6380
alexeyvershinin <og2001@yandex.ru>
2023-02-04 14:45:27 +0300

    Добавил модель с номером и координатами центральной метки участка

--- location/models.py
+++ location/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
+
 # Create your models here.
+class PlotData(models.Model):
+    number = models.CharField(max_length=200, null=True)  # кадастровый номер
+    center_point_x = models.FloatField(null=True)  # координаты центральной метки участка широта
+    center_point_y = models.FloatField(null=True)  # координаты центральной метки участка долгота
