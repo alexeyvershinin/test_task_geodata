36fe685411453292ede1c16a649229e190b60450
alexeyvershinin <og2001@yandex.ru>
2023-02-04 15:47:24 +0300

    Добавил в models.py координаты крайних точек участка

--- location/models.py
+++ location/models.py
@@ -6,3 +6,12 @@ class PlotData(models.Model):
     number = models.CharField(max_length=200, null=True)  # кадастровый номер
     center_point_x = models.FloatField(null=True)  # координаты центральной метки участка широта
     center_point_y = models.FloatField(null=True)  # координаты центральной метки участка долгота
+    # координаты крайних точек участка
+    point_a_x = models.FloatField(null=True)
+    point_a_y = models.FloatField(null=True)
+    point_b_x = models.FloatField(null=True)
+    point_b_y = models.FloatField(null=True)
+    point_c_x = models.FloatField(null=True)
+    point_c_y = models.FloatField(null=True)
+    point_d_x = models.FloatField(null=True)
+    point_d_y = models.FloatField(null=True)
